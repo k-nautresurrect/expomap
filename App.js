939ff984fb8257f92f72cdbbddc7955315fbc586
k-nautresurrect <kushignite@gmail.com>
2021-12-19 09:15:35 +0530

    Added handle function in place of inlines

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("text clicked");
+
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} onPress={() => console.log("text Clicked")}>
+      <Text numberOfLines={1} onPress={handlePress}>
         hey this is a multiline text wiith some long text that may not be
         displayed on the mobile device lets check. it is wrapped automatically
         Ok , fine.
