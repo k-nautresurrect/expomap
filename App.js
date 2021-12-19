17bdbfccdb0729fffba4c0ff09f3280ae9e8885b
k-nautresurrect <kushignite@gmail.com>
2021-12-19 11:08:50 +0530

    Implemented map with expo only

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from "react";
+import MapView from "react-native-maps";
+import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("text clicked");
+  // console.log(Constants.statusBarHeight);
 
   return (
     <View style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        hey this is a multiline text wiith some long text that may not be
-        displayed on the mobile device lets check. it is wrapped automatically
-        Ok , fine.
+      <Text
+        numberOfLines={1}
+        onPress={handlePress}
+        style={{
+          marginTop: Constants.statusBarHeight + 30,
+        }}
+      >
+        Implementing map
       </Text>
+      <MapView style={styles.map} />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,5 +31,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
