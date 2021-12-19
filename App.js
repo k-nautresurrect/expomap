e150a39a99614d510c5cb08a796c383420d83f68
k-nautresurrect <kushignite@gmail.com>
2021-12-19 09:09:40 +0530

    Used text components

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World !</Text>
+      <Text>
+        hey this is a multiline text wiith some long text that may not be
+        displayed on the mobile device lets check. it is wrapped automatically
+        Ok , fine.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
