c77fc1d3c3d6e2d42bc1556167e298572adfeeb5
k-nautresurrect <kushignite@gmail.com>
2021-12-21 10:39:19 +0530

    Added location marker of my location

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Alert,
+  Button,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("text clicked");
-  // console.log(Constants.statusBarHeight);
 
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
@@ -34,28 +41,37 @@ export default function App() {
     })();
   }, []);
 
-  // console.log("mapregion", mapRegion);
+  let text = "Waiting..";
+  let err = null;
+  if (errorMsg) {
+    text = errorMsg;
+    err = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  // make sure that state is changed
+  let markerContainer = null;
+  if (mapRegion) {
+    markerContainer = (
+      <Marker
+        coordinate={mapRegion}
+        title="me"
+        description="my location"
+      ></Marker>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <Text
-        numberOfLines={1}
-        onPress={handlePress}
-        style={{
-          marginTop: Constants.statusBarHeight + 30,
-        }}
-      >
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress} style={styles.heading}>
         Implementing map
       </Text>
       <MapView initialRegion={mapRegion} style={styles.map}>
-        <Marker
-          coordinate={mapRegion}
-          title="me"
-          description="my location"
-        ></Marker>
+        {markerContainer}
       </MapView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -66,8 +82,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  heading: {
+    padding: 20,
+    fontSize: 25,
+    alignSelf: "flex-start",
+  },
   map: {
     width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    height: Dimensions.get("window").height / 2,
   },
 });
