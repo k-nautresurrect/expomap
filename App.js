632aa65efbfd7a94bb17f0e1c33336dd6a009b8b
k-nautresurrect <kushignite@gmail.com>
2021-12-26 10:59:11 +0530

    Reinitialized project

--- App.js
+++ App.js
@@ -14,62 +14,11 @@ import {
 } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("text clicked");
-
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  const [mapRegion, setMapRegion] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      // console.log("location", location);
-
-      setMapRegion({
-        longitude: location.coords.longitude,
-        latitude: location.coords.latitude,
-        longitudeDelta: 0.0922,
-        latitudeDelta: 0.0422,
-      });
-    })();
-  }, []);
-
-  let text = "Waiting..";
-  let err = null;
-  if (errorMsg) {
-    text = errorMsg;
-    err = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  // make sure that state is changed
-  let markerContainer = null;
-  if (mapRegion) {
-    markerContainer = (
-      <Marker
-        coordinate={mapRegion}
-        title="me"
-        description="my location"
-      ></Marker>
-    );
-  }
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress} style={styles.heading}>
+      <Text numberOfLines={1} style={styles.heading}>
         Implementing map
       </Text>
-      <MapView initialRegion={mapRegion} style={styles.map}>
-        {markerContainer}
-      </MapView>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -81,14 +30,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  heading: {
-    padding: 20,
-    fontSize: 25,
-    alignSelf: "flex-start",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height / 2,
   },
 });
